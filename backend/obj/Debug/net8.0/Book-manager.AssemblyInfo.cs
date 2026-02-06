5eb54aa5bf38d8b4cb27d128831b91576bd16508
Vittor-Emanoel <vittoremanoel1234@gmail.com>
2026-02-06 20:13:48 -0300

    feat: move backend to folder

--- backend/obj/Debug/net8.0/Book-manager.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/Book-manager.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Book-manager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4fd4dd3e013091e3453d0412f3248302fdbbc5a4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a379150581594a9435e746685ef43edd1720894e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Book-manager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Book-manager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
